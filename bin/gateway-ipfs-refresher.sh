56233b9d92bfcfd603ad035bfc22da957f275caf
SessionHu <102411014+SessionHu@users.noreply.github.com>
2025-11-22 16:15:03 +0800

    feat(gateway-ipfs-refresher.sh): wait processes

--- bin/gateway-ipfs-refresher.sh
+++ bin/gateway-ipfs-refresher.sh
@@ -1,22 +1,28 @@
-#!/bin/sh
-gateways='
-ipfs.io
-dweb.link
-apac.orbitor.dev
-eu.orbitor.dev
-ipfs.orbitor.dev
-latam.orbitor.dev
-dget.top
-storry.tv
-4everland.io
-nftstorage.link
-ipfs.ecolatam.com
-w3s.link
-gateway.pinata.cloud
-trustless-gateway.link
-'
-for i in $gateways; do
+#!/bin/bash
+gateways=(
+  'ipfs.io'
+  'dweb.link'
+  'apac.orbitor.dev'
+  'eu.orbitor.dev'
+  'ipfs.orbitor.dev'
+  'latam.orbitor.dev'
+  'dget.top'
+  'storry.tv'
+  '4everland.io'
+  'nftstorage.link'
+  'ipfs.ecolatam.com'
+  'w3s.link'
+  'gateway.pinata.cloud'
+  'trustless-gateway.link'
+)
+pids=()
+for i in ${gateways[@]}; do
   wget "https://${i}/ipfs/${1}?format=car" \
+    --timeout=10 --tries=1 \
     -O /dev/null \
     -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' &
+  pids+=($!)
+done
+for i in ${pids[@]}; do
+  wait $i
 done
