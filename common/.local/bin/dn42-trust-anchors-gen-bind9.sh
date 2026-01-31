39d3a7cf9e8260cadf32ed5da0c5af68d314040a
SessionHu <sessionhu@outlook.com>
2026-01-31 21:50:23 +0800

    feat(common): dn42-trust-anchors-gen-bind9.sh

---
+++ common/.local/bin/dn42-trust-anchors-gen-bind9.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SERVER="172.20.0.53"
+DOMAINS="
+dn42.
+20.172.in-addr.arpa.
+21.172.in-addr.arpa.
+22.172.in-addr.arpa.
+23.172.in-addr.arpa.
+10.in-addr.arpa.
+d.f.ip6.arpa.
+"
+
+echo 'trust-anchors {'
+
+for domain in $DOMAINS; do
+  echo "\t# ${domain}"
+  dig "${domain}" DNSKEY @"${SERVER}" +tcp +short | \
+  awk -v domain="${domain}" '{
+    printf "\t\"%s\" static-key %d %d %d \"", domain, $1, $2, $3;
+    for(i=4; i<=NF; i++) {
+      printf "%s", $i
+    };
+    print "\";";
+  }'
+done
+
+echo '};'
